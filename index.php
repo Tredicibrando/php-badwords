4e05efa0ca57c58a68696018ca7e86ffeb62055b
Tredicibrando <nicolosigiov@gmail.com>
2022-03-29 17:00:58 +0200

    Finito

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php
-
-
-$testo = Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, sit!;
-
-
-
+    // testo originale
+    $testo = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, sit!';
+    // conto lunghezza testo originale
+    $lunghezza_originale = strlen($testo);
+    // inserisco badword
+    $badword = $_GET['badword'];
+    // censuro il testo con la parola della query
+    $testo_censurato = str_replace( $badword, 'xxx', $testo)
 ?>
 
 
@@ -19,10 +21,18 @@ $testo = Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, sit!;
 </head>
 <body>
 
+<div>
+<h3>Testo originale</h3>
+<p> <?php echo $testo; ?> </p>
 
-<p>
-    <?php echo $testo ?>
-</p>
+<h3>Lunghezza testo originale</h3>
+<p> <?php echo $lunghezza_originale; ?>  </p>
+
+<h3>Testo censurato</h3>
+<p> <?php echo $testo_censurato ?> </p>
+
+</div>
+
 
 </body>
 </html>
